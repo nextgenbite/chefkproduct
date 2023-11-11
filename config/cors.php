990c9e640855fb0b19cdc6b49ef0cf98ef073b03
nextgenbite <98073256+nextgenbite@users.noreply.github.com>
2023-11-11 11:11:00 +0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','http://ecomnuxt.nextgenbite.xyz'],
+    'allowed_origins' => ['http://localhost:3000','https://ecomnuxt.nextgenbite.xyz'],
 
     'allowed_origins_patterns' => [],
 
