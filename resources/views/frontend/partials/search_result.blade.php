7e1541c0ca6e67417eb95f2c5178e38cab45a8de
nextgenbite <iliusrahman@gmail.com>
2024-07-11 15:04:57 +0600

    s result updated

--- resources/views/frontend/partials/search_result.blade.php
+++ resources/views/frontend/partials/search_result.blade.php
@@ -2,11 +2,11 @@
     
 <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
     <a href="{{ url('/product/'.$item->slug) }}">{{$item->title}} <br>
-        <p class="text-sm lg:text-xl text-red-600 font-roboto font-semibold ">
+        <p class="text-sm  lg:text-xl text-red-600 font-roboto font-semibold ">
             {{ $item->discount ? formatCurrency($item->discount) : formatCurrency($item->price) }}
             @if ($item->discount)
-                <p class="text-sm text-gray-400 font-roboto  line-through "> {{ formatCurrency($item->price) }}
-                </p>
+                <span class="text-sm text-gray-400 font-roboto  line-through "> {{ formatCurrency($item->price) }}
+                </span>
             @endif
         </p>
       </a>
