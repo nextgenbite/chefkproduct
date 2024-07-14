38b0df0946d7cd5e9881bcc6387117df942caa29
nextgenbite <iliusrahman@gmail.com>
2024-07-14 18:26:43 +0600

    #

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,21 +27,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $setting = Setting::all()->pluck('svalue', 'skey');
-        if ($setting) {
-            // config(['cart.tax' => $setting->tax]);
-            View::share('settings', $setting);
-         }
-         $categories =Category::active()->limit(8)->get(['id', 'title','slug', 'icon', 'status']);
-        if ($categories) {
-            // config(['cart.tax' => $setting->tax]);
-            View::share('categories', $categories);
-         }
-         $pages =Page::active()->limit(8)->get(['id', 'title', 'slug', 'status']);
-        if ($pages) {
-            // config(['cart.tax' => $setting->tax]);
-            View::share('pages', $pages);
-         }
+        // $setting = Setting::all()->pluck('svalue', 'skey');
+        // if ($setting) {
+        //     // config(['cart.tax' => $setting->tax]);
+        //     View::share('settings', $setting);
+        //  }
+        //  $categories =Category::active()->limit(8)->get(['id', 'title','slug', 'icon', 'status']);
+        // if ($categories) {
+        //     // config(['cart.tax' => $setting->tax]);
+        //     View::share('categories', $categories);
+        //  }
+        //  $pages =Page::active()->limit(8)->get(['id', 'title', 'slug', 'status']);
+        // if ($pages) {
+        //     // config(['cart.tax' => $setting->tax]);
+        //     View::share('pages', $pages);
+        //  }
 
          
     }
