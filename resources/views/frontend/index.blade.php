681af0c0828372b9d2a7fae1f6efce01fd69eacc
nextgenbite <iliusrahman@gmail.com>
2024-07-16 18:02:36 +0600

    #

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,108 +2,105 @@
 @section('content')
 <!-- ---- Start Banner ----- -->
 @include('frontend.partials.banner')
-               <!-- ---- End Banner ----- -->
+<!-- ---- End Banner ----- -->
 
-                 <!-- ---- Start Features  ----- -->
+<!-- ---- Start Features  ----- -->
 
-          <div class="container py-6 ">
-               <div class="lg:w-10/12 grid md:grid-cols-3 lg:grid-cols-3 grid-cols-1 gap-3 lg:gap-6 mx-auto justify-center ">
-                    <!-- ---- Single Features  ----- -->
-                    <div
-                         class="shadow-lg border border-gray-200 rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
+<div class="container py-6 ">
+     <div class="lg:w-10/12 grid md:grid-cols-3 lg:grid-cols-3 grid-cols-1 gap-3 lg:gap-6 mx-auto justify-center ">
+          <!-- ---- Single Features  ----- -->
+          <div
+               class="shadow-lg border border-gray-200 rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
 
-                         <img src="/images/icons/delivery-van.svg" class="lg:w-12 w-10 h-12 object-contain " />
-                         <div>
-                              <h4 class="font-medium capitalize text-sm lg:text-lg ">Free Shipping </h4>
-                              <p class="text-gray-500 text-xs lg:text-sm "> Order Over ৳500 </p>
-                         </div>
-                    </div>
-                    <!-- ----End  Single Features  ----- -->
-
-
-                    <!-- ---- Single Features  ----- -->
-                    <div
-                         class="shadow-lg border border-gray-200 rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
-
-                         <img src="/images/icons/money-back.svg" class="lg:w-12 w-10 h-12 object-contain " />
-                         <div>
-                              <h4 class="font-medium capitalize text-sm lg:text-lg ">Money Returns </h4>
-                              <p class="text-gray-500 text-xs lg:text-sm "> 14 Days Money Return </p>
-                         </div>
-                    </div>
-                    <!-- ----End  Single Features  ----- -->
-
-
-
-                    <!-- ---- Single Features  ----- -->
-                    <div
-                         class="shadow-lg rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
-
-                         <img src="/images/icons/service-hours.svg" class="lg:w-12 w-10 h-12 object-contain " />
-                         <div>
-                              <h4 class="font-medium capitalize text-sm lg:text-lg">24/7 Support </h4>
-                              <p class="text-gray-500 text-xs lg:text-sm "> Customer Support </p>
-                         </div>
-                    </div>
-                    <!-- ----End  Single Features  ----- -->
+               <img src="/images/icons/delivery-van.svg" class="lg:w-12 w-10 h-12 object-contain " />
+               <div>
+                    <h4 class="font-medium capitalize text-sm lg:text-lg ">Free Shipping </h4>
+                    <p class="text-gray-500 text-xs lg:text-sm "> Order Over ৳500 </p>
                </div>
-
           </div>
+          <!-- ----End  Single Features  ----- -->
 
-          <!-- ---- End Features  ----- -->
 
+          <!-- ---- Single Features  ----- -->
+          <div
+               class="shadow-lg border border-gray-200 rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
 
-                 <!-- ---- Start Top Feature  ----- -->
- 
-                 <div class="mx-2 md:container lg:container" >
-                    <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase   ">
-                         Feature Product 
-                    
-                         <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
-                    </h2>
-                    <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
-                         @forelse ($trendProducts as $product)
-
-                                   @include('frontend.partials.product-x', ['product' => $product])
-                         @empty
-                              <div class="text-red-600 text-center text-bold">No Data Found</div>
-                         @endforelse
-                    </div>
+               <img src="/images/icons/money-back.svg" class="lg:w-12 w-10 h-12 object-contain " />
+               <div>
+                    <h4 class="font-medium capitalize text-sm lg:text-lg ">Money Returns </h4>
+                    <p class="text-gray-500 text-xs lg:text-sm "> 14 Days Money Return </p>
                </div>
-               <hr class=" my-8 bg-gray-200 border-0 dark:bg-gray-700">
-               <!-- ---- End Top Feature  ----- -->
-               <!-- ---- Start Top New Arrival  ----- -->
-               <div class="mx-2 md:container lg:container" >
-                    <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase">
-                         New Arrival 
-                         <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
-                    </h2>
-                    <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
-                         @forelse ($newProducts as $product)
+          </div>
+          <!-- ----End  Single Features  ----- -->
 
-                                   @include('frontend.partials.product-x', ['product' => $product])
-                         @empty
-                              <div class="text-red-600 text-center text-bold">No Data Found</div>
-                         @endforelse
-                    </div>
-               </div>
-               <hr class=" my-8 bg-gray-200 border-0 dark:bg-gray-700">
-               <!-- ---- End Top New Arrival  ----- -->
-               <!-- ---- Start All Products  ----- -->
-               <div class="mx-2 md:container lg:container" >
-                    <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase">
-                         All Products 
-                         <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
-                    </h2>
-                    <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
-                         @forelse ($products as $product)
 
-                                   @include('frontend.partials.product-x', ['product' => $product])
-                         @empty
-                              <div class="text-red-600 text-center text-bold">No Data Found</div>
-                         @endforelse
-                    </div>
+
+          <!-- ---- Single Features  ----- -->
+          <div
+               class="shadow-lg rounded px-3 lg:px-3 lg:py-6 py-2 flex justify-center items-center gap-3 transition hover:border-slate-400 hover:bg-gray-200 duration-300 ">
+
+               <img src="/images/icons/service-hours.svg" class="lg:w-12 w-10 h-12 object-contain " />
+               <div>
+                    <h4 class="font-medium capitalize text-sm lg:text-lg">24/7 Support </h4>
+                    <p class="text-gray-500 text-xs lg:text-sm "> Customer Support </p>
                </div>
-               <hr class=" my-8 bg-gray-200 border-0 dark:bg-gray-700">
-               <!-- ---- End All Products  ----- -->
+          </div>
+          <!-- ----End  Single Features  ----- -->
+     </div>
+
+</div>
+
+<!-- ---- End Features  ----- -->
+
+
+<!-- ---- Start Top Feature  ----- -->
+
+<div class="mx-2 md:container lg:container">
+     <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase   ">
+          Feature Product
+
+          <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
+     </h2>
+     <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
+          @forelse ($trendProducts as $product)
+
+          @include('frontend.partials.product-x', ['product' => $product])
+          @empty
+          <div class="text-red-600 text-center text-bold">No Data Found</div>
+          @endforelse
+     </div>
+</div>
+<!-- ---- End Top Feature  ----- -->
+<!-- ---- Start Top New Arrival  ----- -->
+<div class="mx-2 md:container lg:container">
+     <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase">
+          New Arrival
+          <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
+     </h2>
+     <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
+          @forelse ($newProducts as $product)
+
+          @include('frontend.partials.product-x', ['product' => $product])
+          @empty
+          <div class="text-red-600 text-center text-bold">No Data Found</div>
+          @endforelse
+     </div>
+</div>
+<!-- ---- End Top New Arrival  ----- -->
+<!-- ---- Start All Products  ----- -->
+<div class="mx-2 md:container lg:container">
+     <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase">
+          All Products
+          <hr class="w-1/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
+     </h2>
+     <div class="grid grid-cols-2 lg:grid-cols-5 gap-3">
+          @forelse ($products as $product)
+
+          @include('frontend.partials.product-x', ['product' => $product])
+          @empty
+          <div class="text-red-600 text-center text-bold">No Data Found</div>
+          @endforelse
+     </div>
+</div>
+<!-- ---- End All Products  ----- -->
 @endsection
---
+++ resources/views/frontend/page.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.frontend')
+@section('content')
+
+
+<!-- ---- Start Category  ----- -->
+<div class="mx-2 my-2 py-2 md:container lg:container">
+     <div
+          class="p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h2 class="text-base my-2 md:text-xl font-medium text-gray-800 uppercase">
+               {{$data->title}}
+               <hr class="w-2/3 h-0.5 my-2 bg-gray-200 border-0 dark:bg-gray-700">
+          </h2>
+          <p>{{$data->body}}</p>
+
+     </div>
+
+</div>
+<!-- ---- End Category  ----- -->
+@endsection
