6b461358e278a626bcd86d8c4331b335dbb5ccc9
nextgenbite <iliusrahman@gmail.com>
2024-09-16 10:49:35 +0600

    #

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -239,7 +239,7 @@
             showFrontendAlert(type, message);
         @endif
     //    <!--Start of Tawk.to Script-->
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+/* var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
@@ -248,6 +248,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+*/
 //<!--End of Tawk.to Script-->
 
 
